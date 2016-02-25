39ef03edcd79ddf4e1e15afe6fd9e0edd653b1f4
Bryce Kahle <bkahle@gmail.com>
2016-02-24 17:47:34 -0800

    Remove trailing slash from baseUrl
    Fixes #7

--- src/Defaults.js
+++ src/Defaults.js
@@ -1,5 +1,5 @@
 export default {
-	baseUrl: 'https://api.particle.io/',
+	baseUrl: 'https://api.particle.io',
 	clientSecret: 'particle-api',
 	clientId: 'particle-api',
 	tokenDuration: 7776000, // 90 days
