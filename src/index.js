c945acbb7d0a59f9554bdd25942a1420a5c53267
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-07-21 09:45:45 +0200

    add index file so we can export Particle as the default export, but also provide named exports for supporting classes.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+import Particle from './Particle';
+import Library from './Library';
+import Client from './Client';
+import Agent from './Agent';
+
+
+export default Particle;
+export {
+	Library,
+	Client,
+	Agent
+};
