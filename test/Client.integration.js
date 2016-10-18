b7a0413555c022bec1f53fe6157da6b6c15af932
Matthew McGowan <mat.mcgowan+github@gmail.com>
2016-10-18 16:40:23 +0200

    adds library publish

--- test/Client.integration.js
+++ test/Client.integration.js
@@ -4,8 +4,8 @@ import {read} from './fixtures/index';
 
 describe('Client', () => {
 	describe('A library can be contributed and then published', () => {
-		const auth = process.env.ACCESS_TOKEN || '0aafe7ccb5f7e2b642e7819476b17a610b1d8773';
-		const baseUrl = process.env.API_URLxxx || 'api.staging.particle.io';
+		const auth = process.env.ACCESS_TOKEN;
+		const baseUrl = process.env.API_URL || 'https://api.particle.io';
 		const force = process.env.PARTICLE_LIBRARY_DELETE_TOKEN;
 		if (!force) {
 			throw new Error('PARTICLE_LIBRARY_DELETE_TOKEN should be defined to enable library delete functionality.');
@@ -23,24 +23,25 @@ describe('Client', () => {
 			});
 		}
 
-		// before(deleteTestLibrary);
-		// after(deleteTestLibrary);
+		before(deleteTestLibrary);
+		after(deleteTestLibrary);
 
-
-		xit('cannot publish a library that does not exist', () => {
+		it('cannot publish a library that does not exist', () => {
 			return sut.publishLibrary(name).
 			then(library => {
 				throw Error('expected an exception');
 			}).
 			catch(error => {
-				expect(error.message).to.contain('not found');
+				expect(error.message).to.contain('This library could not be published');
 			});
 		});
 
 		it('can contribute the library', () => {
 			return sut.contributeLibrary(read('test-library-publish-0.0.1.tar.gz'))
-				.then(result => {
-					console.log(result);
+				.then(library => {
+					expect(library.name).to.equal(name);
+					expect(library.version).to.equal('0.0.1');
+					expect(library.visibility).to.equal('private');
 				});
 		});
 
@@ -48,7 +49,7 @@ describe('Client', () => {
 			return sut.library(name).
 			then(library => {
 				expect(library.name).to.equal(name);
-				expect(library.attributes.visibility).to.equal('private');
+				expect(library.visibility).to.equal('private');
 			});
 		});
 
@@ -56,7 +57,7 @@ describe('Client', () => {
 			return sut.publishLibrary(name).
 			then(library => {
 				expect(library.name).to.equal(name);
-				expect(library.attributes.visibility).to.equal('public');
+				expect(library.visibility).to.equal('public');
 			});
 		});
 	});
