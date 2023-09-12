d859697b9b74f4e6e89d4b5060ef31db61e3ed60
CarlosHdez <carloseye11@gmail.com>
2023-09-12 17:00:34 -0600

    Webpack config to generate minified version for browsers

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = (env) => {
+	return {
+		mode: env.mode || 'development',
+		target: 'web',
+		entry: './src/Particle.js',
+		devtool: 'source-map',
+		externals: [nodeExternals()],
+		output: {
+			filename: 'particle.min.js',
+			path: path.resolve(__dirname, 'dist'),
+			clean: true,
+			globalObject: 'this',
+			library: {
+				name: 'particle-api-js',
+				type: 'umd',
+				umdNamedDefine: true,
+				export: 'default'
+			}
+		},
+		optimization: {
+			minimize: true,
+			minimizer: [new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					format: {
+						comments: false
+					}
+				}
+			})]
+		},
+		resolve: {
+			fallback: {
+				buffer: require.resolve('buffer')
+			}
+		},
+		plugins: [
+			new webpack.ProvidePlugin({
+				Buffer: ['buffer', 'Buffer'],
+			})
+		]
+	};
+};
