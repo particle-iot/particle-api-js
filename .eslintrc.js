9f20dfd969d0f985480e6a1236935ef71e0e127d
Cole Abbeduto <cole.abbeduto@particle.io>
2023-12-06 13:28:39 -0600

    Switch from tabs to spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,25 @@
 module.exports = {
-	extends: ['eslint-config-particle'],
-	parserOptions: {
-		sourceType: 'module'
-	},
-	env: {
-		browser: true,
-		commonjs: true,
-		es6: true,
-		node: true,
-		mocha: true,
-		worker: true,
-		serviceworker: true
-	},
-	rules: {
-		'no-prototype-builtins': 'off',
-		'no-redeclare': 'off',
-		camelcase: ['error', {
-			properties: 'never',
-			allow: ['redirect_uri']
-		}]
-	}
+    extends: ['eslint-config-particle'],
+    parserOptions: {
+        sourceType: 'module'
+    },
+    env: {
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
+        mocha: true,
+        worker: true,
+        serviceworker: true
+    },
+    rules: {
+        'no-prototype-builtins': 'off',
+        'no-redeclare': 'off',
+        camelcase: ['error', {
+            properties: 'never',
+            allow: ['redirect_uri']
+        }],
+        'no-mixed-spaces-and-tabs': 'error',
+        indent: ['error', 4]
+    }
 };
